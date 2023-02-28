c9f6808fa0390937ba89114de0509fd2cba6a592
kimyerim1935 <kimyerim1935@naver.com>
2023-02-28 10:57:29 +0900

    init: msw handler 셋팅

---
+++ src/mocks/handers.ts
@@ -0,0 +1,8 @@
+// handlers.ts
+/**
+ * 모킹하려하는 API를 가져와 handlers에 넣어줍니다.
+ * handler를 따로 분리하는 이유는 연관된 API끼리 묶어주기 위함입니다.
+ */
+import searchResultHandler from "mocks/api/searchResult";
+
+export const handlers = [...searchResultHandler];
