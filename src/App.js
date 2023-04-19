8dbea7a9ba7fd7fecb65124f651f581ff1c9538e
doRnsns98 <61038091+Kimyerim1935@users.noreply.github.com>
2023-04-19 17:46:02 +0900

    mod: 상세페이지 url 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MovieList from './routes/MovieList';
+import Movie from './routes/Movie';
+import client from './client';
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<MovieList />} />
+                <Route path="/movies/:id" element={<Movie />} />
             </Routes>
         </BrowserRouter>
     );
