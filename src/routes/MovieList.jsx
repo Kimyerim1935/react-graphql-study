9a6d0ce5d0f2641a3bd3ce9eceff50cbaa3fd9e8
doRnsns98 <61038091+Kimyerim1935@users.noreply.github.com>
2023-04-20 17:00:06 +0900

    mod: data 호출

--- src/routes/MovieList.jsx
+++ src/routes/MovieList.jsx
@@ -1,3 +1,25 @@
+import { gql, useApolloClient } from '@apollo/client';
+import { useEffect, useState } from 'react';
+
 export default function MovieList() {
+    const client = useApolloClient();
+
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        client
+            .query({
+                query: gql`
+                    {
+                        allMoview {
+                            title
+                        }
+                    }
+                `,
+            })
+            .then((results) => setMovies(results));
+    }, [client]);
+
+    console.log(movies);
     return <>this is MovieList</>;
 }
