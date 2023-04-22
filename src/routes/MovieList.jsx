f252ae562a370bd2f6785f1e5ad0e1b1e46c20f2
김예림 <kimyerim1935@naver.com>
2023-04-22 23:45:41 +0900

    mod: getData

--- src/routes/MovieList.jsx
+++ src/routes/MovieList.jsx
@@ -1,28 +1,38 @@
-import { gql, useApolloClient, useQuery } from '@apollo/client';
-import { useEffect, useState } from 'react';
+import { gql, useQuery } from '@apollo/client';
+
+const ALL_MOVIES = gql`
+    query getMovies{
+        allMovies {
+            title
+            id
+        }
+        allTweets {
+            id
+            text
+            author {
+                fullName
+            }
+        }
+    }
+`;
 
 export default function MovieList() {
-    const client = useApolloClient();
-
-    const [movies, setMovies] = useState([]);
-
-    useEffect(() => {
-        client
-            .query({
-                query: gql`
-                    {
-                        allMoview {
-                            title
-                        }
-                    }
-                `,
-            })
-            .then((results) => setMovies(results.data.allMovies));
-    }, [client]);
-
-    return (
-        <div>
-            {movies.map((el) => <li key={el.id}>{el.title}</li>)}
-        </div>
+    const { data, loading, error } = useQuery(ALL_MOVIES);
+    console.log(data.allTweets);
+    
+    if (loading) {
+        return <h1>loading......</h1>
+    } 
+    if (error) {
+        return <h1>{error}</h1>
+    }
+    else return (
+        <>
+            {loading ? <span>LOADING.....</span>: (
+            <ul>
+                {data?.allMovies?.map((el) => <li key={el.id}>{el.title}</li>)}
+            </ul>
+            )}
+        </>
     );
 }
