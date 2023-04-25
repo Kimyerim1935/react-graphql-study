ff5f40965b7af6b556935c51e36d79fc625df710
doRnsns98 <61038091+Kimyerim1935@users.noreply.github.com>
2023-04-25 18:11:18 +0900

    mod: params

--- src/routes/MovieList.jsx
+++ src/routes/MovieList.jsx
@@ -1,38 +1,37 @@
 import { gql, useQuery } from '@apollo/client';
+import { Link } from 'react-router-dom';
 
 const ALL_MOVIES = gql`
-    query getMovies{
+    query getMovies {
         allMovies {
             title
             id
-        }
-        allTweets {
-            id
-            text
-            author {
-                fullName
-            }
         }
     }
 `;
 
 export default function MovieList() {
     const { data, loading, error } = useQuery(ALL_MOVIES);
-    console.log(data.allTweets);
-    
+
     if (loading) {
-        return <h1>loading......</h1>
-    } 
-    if (error) {
-        return <h1>{error}</h1>
+        return <h1>loading......</h1>;
     }
-    else return (
-        <>
-            {loading ? <span>LOADING.....</span>: (
-            <ul>
-                {data?.allMovies?.map((el) => <li key={el.id}>{el.title}</li>)}
-            </ul>
-            )}
-        </>
-    );
+    if (error) {
+        return <h1>{error}</h1>;
+    } else
+        return (
+            <>
+                {loading ? (
+                    <span>LOADING.....</span>
+                ) : (
+                    <ul>
+                        {data?.allMovies?.map((el) => (
+                            <li key={el.id}>
+                                <Link to={`/movies/${el.id}`}>{el.title}</Link>
+                            </li>
+                        ))}
+                    </ul>
+                )}
+            </>
+        );
 }
