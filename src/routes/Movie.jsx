e5b27b7b4205127de3d51bf94175bc5eb7d6e95c
김예림 <61038091+Kimyerim1935@users.noreply.github.com>
2023-04-28 19:56:24 +0900

    mod: movie getData

--- src/routes/Movie.jsx
+++ src/routes/Movie.jsx
@@ -1,7 +1,32 @@
 import { useParams } from 'react-router-dom';
+import { gql, useQuery } from '@apollo/client';
+
+const GET_MOVIE = gql`
+    query getMovie($movieId: String!) {
+        movie(id: $movieId) {
+            id
+            title
+        }
+    }
+`
 
 export default function Movie() {
-    const params = useParams();
+    const { id } = useParams();
+    const { loading, data } = useQuery(GET_MOVIE, {
+        variables: {
+            movieId: id
+        }
+    });
+    if (loading) {
+        return
+    }
+    console.log(data, loading);
 
-    return <>this is movie: {params}</>;
+    return (
+        <>
+            {
+                loading ?  <h1>Fetching........ movie</h1> : <h3>this is movie</h3>
+            }
+        </>
+    )
 }
